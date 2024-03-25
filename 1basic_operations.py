a533dff293ab079216f1475c1a97714c465e08ac
Eric Yisroel Reiss <yireiss@gmail.com>
2024-03-25 21:00:33 +0200

    Create 1basic_operations.py
    Exercises for Basic Operations of LS Into to Programming Python book.

---
+++ 1basic_operations.py
@@ -0,0 +1,20 @@
+# 1. Concatenate two strings, one with your first name and one with your last, to create a new string with your full name as its value. 
+# For example, if your name is John Doe, you should combine 'John' and 'Doe' to get 'John Doe'.
+
+first_name = "Eric" 
+last_name = "Reiss"
+
+print(f'{first_name} {last_name}')
+
+# 3. What does the following code do? Why?
+# print('5' + '10')
+
+print('510')
+
+# 4. Refactor the code from the previous exercise to use coercion to print 15 instead.
+
+print(int('5') + int('10'))
+
+
+
+
