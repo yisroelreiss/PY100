c667c4faf7178e84896073b9b01ee9c362716a09
Eric Yisroel Reiss <yireiss@gmail.com>
2024-03-31 20:06:03 +0300

    input_output

---
+++ book_exercises/input_output/ex1.py
@@ -0,0 +1,8 @@
+# greeter.py
+
+name = input("What is your name? ")
+print(f'Hello, {name}!')
+
+# Other Possibilities
+# name = input('What is your name? ')
+# print('Hello, ' + name + '!')
---
+++ book_exercises/input_output/ex2.py
@@ -0,0 +1,5 @@
+# greeter2.py
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+print(f'Hello, {first_name} {last_name}!')
